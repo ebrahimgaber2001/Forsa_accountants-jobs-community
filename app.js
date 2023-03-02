ad4b9f8c383bff514659dd357d275f3021e347c4
ahmedsalah2002 <ahmed147salah@gmail.com>
2023-03-02 14:49:52 +0200

    asd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT ;
+const port = process.env.PORT || 5000 ;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
